3245153d41da1d89c32459197f947da713aadf39
Mikhail Gorbunov <toplenboren@gmail.com>
2023-12-05 19:57:20 +0500

    remove obvious stuff

--- cv.py
+++ cv.py
@@ -3,15 +3,12 @@ import numpy as np
 
 ### START SETUP
 
-# Background Subtractor settings
 BG_SUB_HISTORY = 500
 BG_SUB_VAR_THRESHOLD = 10
 BG_SUB_DETECT_SHADOWS = False
 
-# Minimum area threshold for a valid moving object
 MIN_CONTOUR_AREA = 40000
 
-# Smoothing factor for the dot's movement (adjust as needed)
 SMOOTHING_FACTOR = 0.1
 
 KSIZE = (7, 7)
@@ -47,7 +44,6 @@ def get_new_center(bounding_rect, prev_smoothed_center=None):
 
 
 while True:
-    # Read a frame from the video feed
     ret, frame = cap.read()
     if not ret:
         break
@@ -60,13 +56,10 @@ while True:
     fg_mask = cv2.morphologyEx(fg_mask_raw, cv2.MORPH_OPEN, kernel)
     fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, kernel)
 
-    # Find contours in the foreground mask
     contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Filter contours based on minimum area
     valid_contours = [contour for contour in contours if cv2.contourArea(contour) > MIN_CONTOUR_AREA]
 
-    # Find the contour with the maximum area
     max_contour = max(valid_contours, key=cv2.contourArea, default=None)
 
     if max_contour is not None:
@@ -101,10 +94,8 @@ while True:
         ))
     ))
 
-    # Display the result frame with the largest moving object rectangle
     cv2.imshow("Moving Object Detection", display_img)
 
-    # Break the loop when 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
